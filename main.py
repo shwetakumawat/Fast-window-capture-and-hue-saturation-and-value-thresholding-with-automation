97eab5827d5dcb5c2e7c3b8d181fb4fe4cb03143
Shweta_kumawat <52671257+shwetakumawat@users.noreply.github.com>
2021-08-31 23:06:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,76 +1,69 @@
 import cv2 as cv
 import numpy as np
 import os
-from time import time
-from windowcapture import WindowCapture
-from detection import Detection
-from vision import Vision
-from bot import AlbionBot, BotState
+
 
 # Change the working directory to the folder this script is in.
-# Doing this because I'll be putting the files from each video in their 
-# own folder on GitHub
+# Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# Can use IMREAD flags to do different pre-processing of image files,
+# like making them grayscale or reducing the size.
+# https://docs.opencv.org/4.2.0/d4/da8/group__imgcodecs.html
+haystack_img = cv.imread('albion_farm.jpg', cv.IMREAD_UNCHANGED)
+needle_img = cv.imread('albion_cabbage.jpg', cv.IMREAD_UNCHANGED)
 
-DEBUG = True
+# There are 6 comparison methods to choose from:
+# TM_CCOEFF, TM_CCOEFF_NORMED, TM_CCORR, TM_CCORR_NORMED, TM_SQDIFF, TM_SQDIFF_NORMED
+# You can see the differences at a glance here:
+# https://docs.opencv.org/master/d4/dc6/tutorial_py_template_matching.html
+# Note that the values are inverted for TM_SQDIFF and TM_SQDIFF_NORMED
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
 
-# initialize the WindowCapture class
-wincap = WindowCapture('Albion Online Client')
-# load the detector
-detector = Detection('limestone_model_final.xml')
-# load an empty Vision class
-vision = Vision()
-# initialize the bot
-bot = AlbionBot((wincap.offset_x, wincap.offset_y), (wincap.w, wincap.h))
+# You can view the result of matchTemplate() like this:
+#cv.imshow('Result', result)
+#cv.waitKey()
+# If you want to save this result to a file, you'll need to normalize the result array
+# from 0..1 to 0..255, see:
+# https://stackoverflow.com/questions/35719480/opencv-black-image-after-matchtemplate
+#cv.imwrite('result_CCOEFF_NORMED.jpg', result * 255)
 
-wincap.start()
-detector.start()
-bot.start()
+# Get the best match position from the match result.
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+# The max location will contain the upper left corner pixel position for the area
+# that most closely matches our needle image. The max value gives an indication
+# of how similar that find is to the original needle, where 1 is perfect and -1
+# is exact opposite.
+print('Best match top left position: %s' % str(max_loc))
+print('Best match confidence: %s' % max_val)
 
-while(True):
+# If the best match value is greater than 0.8, we'll trust that we found a match
+threshold = 0.8
+if max_val >= threshold:
+    print('Found needle.')
 
-    # if we don't have a screenshot yet, don't run the code below this point yet
-    if wincap.screenshot is None:
-        continue
+    # Get the size of the needle image. With OpenCV images, you can get the dimensions 
+    # via the shape property. It returns a tuple of the number of rows, columns, and 
+    # channels (if the image is color):
+    needle_w = needle_img.shape[1]
+    needle_h = needle_img.shape[0]
 
-    # give detector the current screenshot to search for objects in
-    detector.update(wincap.screenshot)
+    # Calculate the bottom right corner of the rectangle to draw
+    top_left = max_loc
+    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
 
-    # update the bot with the data it needs right now
-    if bot.state == BotState.INITIALIZING:
-        # while bot is waiting to start, go ahead and start giving it some targets to work
-        # on right away when it does start
-        targets = vision.get_click_points(detector.rectangles)
-        bot.update_targets(targets)
-    elif bot.state == BotState.SEARCHING:
-        # when searching for something to click on next, the bot needs to know what the click
-        # points are for the current detection results. it also needs an updated screenshot
-        # to verify the hover tooltip once it has moved the mouse to that position
-        targets = vision.get_click_points(detector.rectangles)
-        bot.update_targets(targets)
-        bot.update_screenshot(wincap.screenshot)
-    elif bot.state == BotState.MOVING:
-        # when moving, we need fresh screenshots to determine when we've stopped moving
-        bot.update_screenshot(wincap.screenshot)
-    elif bot.state == BotState.MINING:
-        # nothing is needed while we wait for the mining to finish
-        pass
+    # Draw a rectangle on our screenshot to highlight where we found the needle.
+    # The line color can be set as an RGB tuple
+    cv.rectangle(haystack_img, top_left, bottom_right, 
+                    color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
 
-    if DEBUG:
-        # draw the detection results onto the original image
-        detection_image = vision.draw_rectangles(wincap.screenshot, detector.rectangles)
-        # display the images
-        cv.imshow('Matches', detection_image)
+    # You can view the processed screenshot like this:
+    #cv.imshow('Result', haystack_img)
+    #cv.waitKey()
+    # Or you can save the results to a file.
+    # imwrite() will smartly format our output image based on the extension we give it
+    # https://docs.opencv.org/3.4/d4/da8/group__imgcodecs.html#gabbc7ef1aa2edfaa87772f1202d67e0ce
+    cv.imwrite('result.jpg', haystack_img)
 
-    # press 'q' with the output window focused to exit.
-    # waits 1 ms every loop to process key presses
-    key = cv.waitKey(1)
-    if key == ord('q'):
-        wincap.stop()
-        detector.stop()
-        bot.stop()
-        cv.destroyAllWindows()
-        break
-
-print('Done.')
+else:
+    print('Needle not found.')
